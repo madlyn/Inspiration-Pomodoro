d578d1b8dcc7495af2f3f35d82d23fbf933ebb5c
Lyn Al Masri <lynmasry@gmail.com>
2018-11-14 20:27:26 +0300

    Timer settings

--- Insperation Pomodoro/View Controllers/SettingsViewController.swift
+++ Insperation Pomodoro/View Controllers/SettingsViewController.swift
@@ -22,6 +22,24 @@ class SettingsViewController: UIViewController {
     
     
     @IBAction func donePressed(_ sender: Any) {
+        if let work = workSessionTextField.text{
+            let workTime = Float(work)
+            if workTime != nil{
+                UserDefaults.standard.set(workTime, forKey: "WorkSession")
+            }
+        }
+        if let longBreak = longBreakTextField.text{
+            let longBreakTime = Float(longBreak)
+            if longBreakTime != nil{
+                UserDefaults.standard.set(longBreakTime, forKey: "LongBreak")
+            }
+        }
+        if let shortBreak = shortBreakTextField.text{
+            let shortBreakTime = Float(shortBreak)
+            if shortBreakTime != nil{
+                UserDefaults.standard.set(shortBreakTime, forKey: "ShortBreak")
+            }
+        }
         self.dismiss(animated: true, completion: nil)
     }
     
